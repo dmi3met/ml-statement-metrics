6a5b7481fb3a0036f0ce256d9a3a422942b77838
Dmitrii Metelkov <ioniffer@mail.ru>
2019-07-12 14:11:24 +0400

    added first and second answer

--- statement-metrics.py
+++ statement-metrics.py
@@ -1,14 +1,31 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, \
+    f1_score, precision_recall_curve
 import pandas as pd
 
 data = pd.read_csv('classification.csv')
 y_true = data.iloc[:,0]
 y_pred = data.iloc[:,1]
 
-first_answer = [accuracy_score(y_true, y_pred),
+tp = fp = fn = tn = 0
+
+for i in range(len(y_true)):
+    if y_true[i]:
+        if y_pred[i]:
+            tp += 1
+        else:
+            fn += 1
+    else:
+        if y_pred[i]:
+            fp += 1
+        else:
+            tn += 1
+first_answer = [tp, fp, fn, tn]
+print(first_answer)
+second_answer = [accuracy_score(y_true, y_pred),
                 precision_score(y_true, y_pred),
                 recall_score(y_true, y_pred),
                 f1_score(y_true, y_pred)]
-print(first_answer)
+print(second_answer)
 
-data = 
+data2 = pd.read_csv('scores.csv')
+y2_true = data2.iloc[:,0]
